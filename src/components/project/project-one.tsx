22e2c2815b892e45c13820103c66ff1adb1dd2e1
mateounamuno <mateo@unamunoarq.com>
2025-09-06 11:00:58 -0300

    imagen service escritorio

--- src/components/project/project-one.tsx
+++ src/components/project/project-one.tsx
@@ -259,7 +259,7 @@ const ProjectOne = ({ style_2 = false }: IProps) => {
                     data-speed="auto"
                     style={{
                       backgroundImage:
-                        "url(/assets/img/inner-service/hero/hero-1-2.jpg)",
+                        "url(/assets/img/inner-service/hero/hero-2.jpg)",
                     }}
                   ></div>
                 </div>
