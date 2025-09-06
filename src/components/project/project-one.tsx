f253ff7dc0fac8dc84e0744a3cee3551a9083986
mateounamuno <mateo@unamunoarq.com>
2025-09-06 11:29:26 -0300

    ver proyecto cuando pongo el mouse

--- src/components/project/project-one.tsx
+++ src/components/project/project-one.tsx
@@ -117,7 +117,7 @@ function ProjectItem({ item, isLast }: { item: IProject; isLast: boolean }) {
               left: 0,
               right: 0,
               bottom: 0,
-              background: 'rgba(0, 0, 0, 0.3)',
+              background: 'rgba(0, 0, 0, 0.2)',
               opacity: 0,
               transition: 'opacity 0.3s ease',
               display: 'flex',
