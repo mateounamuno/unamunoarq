f145413ccd628fe6da469f63e25f65c7e7495c87
mateounamuno <mateo@unamunoarq.com>
2025-09-12 18:07:18 -0300

    big text portfolio grid updated

--- src/components/big-text.tsx
+++ src/components/big-text.tsx
@@ -6,13 +6,13 @@ type IProps = {
   cls?: string;
 };
 
-export default function BigText({cls=""}: IProps) {
+export default function BigText({ cls = "" }: IProps) {
   return (
     <div className={`sv-big-text-area pb-80 ${cls}`}>
       <div className="container container-1530">
         <div className="sv-small-text-box d-flex justify-content-between">
-          <span>DIGITAL DESIGN EXPERIENCE</span>
-          <span>CREATIVE STUDIO</span>
+          <span>COMENZÁ TU PROYECTO</span>
+          <span>UNAMUNO ARQ.</span>
         </div>
         <div className="sv-big-text-box">
           <h4 className="sv-big-text tp-char-animation">
