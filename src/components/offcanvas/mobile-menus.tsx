306b862712d71a6c591a171a81354eff92732e8b
mateounamuno <mateo@unamunoarq.com>
2025-09-25 16:07:31 -0300

    offcanvas menu mobile updated

--- src/components/offcanvas/mobile-menus.tsx
+++ src/components/offcanvas/mobile-menus.tsx
@@ -7,6 +7,7 @@ import port_img from '@/assets/img/menu/portfolio-menu/portfolio.png';
 
 export default function MobileMenus() {
   const [navTitle, setNavTitle] = React.useState<string>("");
+  const [expandedCategories, setExpandedCategories] = React.useState<string[]>([]);
 
   //openMobileMenu
   const openMobileMenu = (menu: string) => {
@@ -16,6 +17,15 @@ export default function MobileMenus() {
       setNavTitle(menu);
     }
   };
+
+  //toggleCategory
+  const toggleCategory = (category: string) => {
+    setExpandedCategories(prev =>
+      prev.includes(category)
+        ? prev.filter(cat => cat !== category)
+        : [...prev, category]
+    );
+  };
   return (
     <>
       <nav className="tp-main-menu-content">
@@ -23,194 +33,105 @@ export default function MobileMenus() {
           {menu_data.map((menu) => (
             <li
               key={menu.id}
-              className={`has-dropdown ${
-                menu.home_menus || menu.portfolio_mega_menus
-                  ? "has-homemenu"
-                  : ""
-              } ${menu.home_menus ? "dropdown-opened" : ""}`}
+              className={`has-dropdown ${menu.home_menus || menu.portfolio_mega_menus || menu.pages_mega_menu || menu.dropdown_menus
+                ? "has-dropdown"
+                : ""
+                }`}
             >
-              <a className="pointer" onClick={() => openMobileMenu(menu.title)}>
-                {menu.title}
-                <button className="dropdown-toggle-btn">
-                  <i className="fa-light fa-plus"></i>
-                </button>
-              </a>
+              {(menu.home_menus || menu.portfolio_mega_menus || menu.pages_mega_menu || menu.dropdown_menus) ? (
+                <a className="pointer" onClick={() => openMobileMenu(menu.title)}>
+                  {menu.title}
+                  <button className="dropdown-toggle-btn">
+                    <i className={`fa-light ${navTitle === menu.title ? "fa-minus" : "fa-plus"}`}></i>
+                  </button>
+                </a>
+              ) : (
+                <Link href={menu.link} className="nav-link">
+                  {menu.title}
+                </Link>
+              )}
               {menu.home_menus ? (
-                <div className="tp-submenu submenu tp-mega-menu" style={{ display: navTitle === menu.title ? "block" : "none"}}>
-                  <div className="tp-menu-fullwidth">
-                    <div className="tp-homemenu-wrapper">
-                      <div className="row gx-25 row-cols-xl-6 row-cols-lg-2 row-cols-md-2 row-cols-1">
-                        {menu.home_menus.map((hm, i) => (
-                          <div key={i} className="col homemenu">
-                            <div className="homemenu-thumb-wrap mb-20">
-                              <div className="homemenu-thumb fix">
-                                <Link href={hm.link}>
-                                  <Image src={hm.img} alt={hm.title} width={512} height={480} style={{ height: "100%" }} />
-                                </Link>
-                              </div>
-                            </div>
-                            <div className="homemenu-content text-center">
-                              <h4 className="homemenu-title">
-                                <Link href={hm.link}>{hm.title}</Link>
-                              </h4>
-                            </div>
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <ul className="tp-submenu submenu" style={{ display: navTitle === menu.title ? "block" : "none" }}>
+                  {menu.home_menus.map((hm, i) => (
+                    <li key={i}>
+                      <Link href={hm.link}>{hm.title}</Link>
+                    </li>
+                  ))}
+                </ul>
               ) : menu.pages_mega_menu ? (
-                <div className="tp-submenu submenu tp-mega-menu" style={{ display: navTitle === menu.title ? "block" : "none"}}>
-                  <div className="tp-megamenu-wrapper">
-                    <div className="row gx-50">
-                      <div className="col-xl-8">
-                        <div className="tp-megamenu-list-box">
-                          <div className="row gx-50">
-                            <div className="col-xl-8">
-                              <div className="tp-megamenu-list">
-                                <h4 className="tp-megamenu-title">
-                                  {menu.pages_mega_menu.first.title}
-                                </h4>
-                                <div className="tp-megamenu-list-wrap">
-                                  <ul>
-                                    {menu.pages_mega_menu.first.submenus.map(
-                                      (sm, i) => (
-                                        <li key={i}>
-                                          <Link href={sm.link}>{sm.title}</Link>
-                                        </li>
-                                      )
-                                    )}
-                                  </ul>
-                                </div>
-                              </div>
-                            </div>
-                            <div className="col-xl-4">
-                              <div className="tp-megamenu-list tp-megamenu-list-2">
-                                <h4 className="tp-megamenu-title">
-                                  {menu.pages_mega_menu.second.title}
-                                </h4>
-                                <div className="tp-megamenu-list-wrap">
-                                  <ul>
-                                    {menu.pages_mega_menu.second.submenus.map(
-                                      (sm, i) => (
-                                        <li key={i}>
-                                          <Link href={sm.link}>{sm.title}</Link>
-                                        </li>
-                                      )
-                                    )}
-                                  </ul>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-xl-4">
-                        <div className="tp-megamenu-shop-style">
-                          <div className="tp-shop-banner-left p-relative">
-                            <div className="tp-shop-banner-thumb">
-                              <Image
-                                src={shop_banner}
-                                alt="shop-banner"
-                                style={{height:'auto'}}
-                              />
-                            </div>
-                            <div className="tp-shop-banner-content">
-                              <h4 className="tp-shop-banner-title">Sale</h4>
-                              <span>20% Off all Shoes</span>
-                              <Link
-                                className="tp-shop-btn"
-                                href="/shop-details/1"
-                              >
-                                Shop Now
-                              </Link>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <ul className="tp-submenu submenu" style={{ display: navTitle === menu.title ? "block" : "none" }}>
+                  <li>
+                    <h4 className="tp-megamenu-title">
+                      {menu.pages_mega_menu.first.title}
+                    </h4>
+                    <ul>
+                      {menu.pages_mega_menu.first.submenus.map((sm, i) => (
+                        <li key={i}>
+                          <Link href={sm.link}>{sm.title}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                  <li>
+                    <h4 className="tp-megamenu-title">
+                      {menu.pages_mega_menu.second.title}
+                    </h4>
+                    <ul>
+                      {menu.pages_mega_menu.second.submenus.map((sm, i) => (
+                        <li key={i}>
+                          <Link href={sm.link}>{sm.title}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                </ul>
               ) : menu.portfolio_mega_menus ? (
-                <div className="tp-submenu submenu tp-mega-menu" style={{ display: navTitle === menu.title ? "block" : "none"}}>
-                  <div className="tp-menu-fullwidth">
-                    <div className="tp-megamenu-portfolio p-relative">
-                      <div className="tp-megamenu-portfolio-banner">
-                        <Image
-                          src={port_img}
-                          alt="port-img"
-                          style={{height:'auto'}}
-                        />
-                      </div>
-                      <div className="row gx-50">
-                        <div className="col-xxl-9 col-xl-10">
-                          <div className="tp-megamenu-list-box">
-                            <div className="row gx-50">
-                              <div className="col-xxl-5 col-xl-6">
-                                <div className="tp-megamenu-list">
-                                  <h4 className="tp-megamenu-title">
-                                    {menu.portfolio_mega_menus.first.title}
-                                  </h4>
-                                  <div className="tp-megamenu-list-wrap tp-portfolio-menu-style">
-                                    <div className="row">
-                                      {menu.portfolio_mega_menus.first.submenus.map(
-                                        (portSm, i) => (
-                                          <div key={i} className="col-lg-6">
-                                            <ul>
-                                              {portSm.menu_lists.map((psm) => (
-                                                <li key={psm.title}>
-                                                  <Link href={psm.link}>
-                                                    {psm.title}
-                                                  </Link>
-                                                </li>
-                                              ))}
-                                            </ul>
-                                          </div>
-                                        )
-                                      )}
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-
-                              {menu.portfolio_mega_menus.second.submenus.map(
-                                (portSm2, i) => (
-                                  <div key={i} className="col-xxl-3 col-xl-3">
-                                    <div className="tp-megamenu-list tp-megamenu-list-2 ml-20">
-                                      <h4 className="tp-megamenu-title">
-                                        {portSm2.title}
-                                      </h4>
-                                      <div className="tp-megamenu-list-wrap">
-                                        <ul>
-                                          {portSm2.menu_lists.map((psm) => (
-                                            <li key={psm.title}>
-                                              <Link href={psm.link}>
-                                                {psm.title}
-                                              </Link>
-                                            </li>
-                                          ))}
-                                        </ul>
-                                      </div>
-                                    </div>
-                                  </div>
-                                )
-                              )}
-                            </div>
-                          </div>
-                        </div>
-                        <div className="col-xxl-3 col-xl-3 d-none d-xxl-block">
-                          <div className="tp-megamenu-portfolio-text">
-                            <h4>60+</h4>
-                            <span>Pre-built demo home page</span>
-                          </div>
-                        </div>
-                      </div>
+                <ul className="tp-submenu submenu" style={{ display: navTitle === menu.title ? "block" : "none" }}>
+                  <li style={{ marginTop: '15px' }}>
+                    <div className="category-header" onClick={() => toggleCategory(menu.portfolio_mega_menus?.first.title || '')} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', cursor: 'pointer', padding: '10px 0' }}>
+                      <h4 className="tp-megamenu-title" style={{ margin: 0 }}>
+                        {menu.portfolio_mega_menus?.first.title}
+                      </h4>
+                      <button className="dropdown-toggle-btn" style={{ border: 'none', background: 'none', cursor: 'pointer' }}>
+                        <i className={`fa-light ${expandedCategories.includes(menu.portfolio_mega_menus?.first.title || '') ? "fa-minus" : "fa-plus"}`}></i>
+                      </button>
                     </div>
-                  </div>
-                </div>
+                    <ul style={{ display: expandedCategories.includes(menu.portfolio_mega_menus?.first.title || '') ? "block" : "none" }}>
+                      {menu.portfolio_mega_menus?.first.submenus.map((portSm, i) => (
+                        <li key={i}>
+                          {portSm.menu_lists.map((psm) => (
+                            <Link key={psm.title} href={psm.link}>
+                              {psm.title}
+                            </Link>
+                          ))}
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                  {menu.portfolio_mega_menus.second.submenus.map((portSm2, i) => (
+                    <li key={i} style={{ marginTop: '15px' }}>
+                      <div className="category-header" onClick={() => toggleCategory(portSm2.title)} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', cursor: 'pointer', padding: '10px 0' }}>
+                        <h4 className="tp-megamenu-title" style={{ margin: 0 }}>
+                          {portSm2.title}
+                        </h4>
+                        <button className="dropdown-toggle-btn" style={{ border: 'none', background: 'none', cursor: 'pointer' }}>
+                          <i className={`fa-light ${expandedCategories.includes(portSm2.title) ? "fa-minus" : "fa-plus"}`}></i>
+                        </button>
+                      </div>
+                      <ul style={{ display: expandedCategories.includes(portSm2.title) ? "block" : "none" }}>
+                        {portSm2.menu_lists.map((psm) => (
+                          <li key={psm.title}>
+                            <Link href={psm.link}>
+                              {psm.title}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </li>
+                  ))}
+                </ul>
               ) : menu.dropdown_menus ? (
-                <ul className="tp-submenu submenu" style={{ display: navTitle === menu.title ? "block" : "none"}}>
+                <ul className="tp-submenu submenu" style={{ display: navTitle === menu.title ? "block" : "none" }}>
                   {menu.dropdown_menus.map((mm, i) => (
                     <li key={i}>
                       <Link href={mm.link}>{mm.title}</Link>
