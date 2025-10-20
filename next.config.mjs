14dd82b899d8204d6b58a636e02b8e8723887f98
mateounamuno <mateo@unamunoarq.com>
2025-10-20 16:29:51 -0300

    error de deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        formats: ['image/webp', 'image/avif'],
+    },
+    experimental: {
+        optimizePackageImports: ['gsap', 'three'],
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: false,
+    },
+};
 
 export default nextConfig;
