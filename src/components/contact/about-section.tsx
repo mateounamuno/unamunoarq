cd9115405e478df77d9aa8b5101e0f41c07c2a07
mateounamuno <mateo@unamunoarq.com>
2025-12-15 13:11:11 +1100

    bio updated

--- src/components/contact/about-section.tsx
+++ src/components/contact/about-section.tsx
@@ -24,7 +24,7 @@ const AboutSection = () => {
                         </div>
 
                         {/* Right column - Image */}
-                        <div className="col-xl-6 col-lg-6 col-md-12 about-image-col" style={{ height: "100%" }}>
+                        <div className="col-xl-6 col-lg-6 col-md-12 about-image-col" style={{ height: "100%", padding: "0" }}>
                             <div className="contact-about-image about-image-wrap tp_img_fade_in" data-delay="0" style={{ width: "100%", height: "80vh", overflow: "hidden" }}>
                                 <Image
                                     src="/assets/img/inner-contact/contact/oficina.jpg"
@@ -47,7 +47,7 @@ const AboutSection = () => {
                         <div className="col-xl-6 col-lg-6 col-md-12 bio-img-col" style={{ height: "100%" }}>
                             <div className="  about-image-wrap tp_img_fade_in" data-delay="0" style={{ width: "100%", height: "80vh", overflow: "hidden" }}>
                                 <Image
-                                    src="/assets/img/inner-contact/contact/oficina.jpg"
+                                    src="/assets/img/inner-contact/contact/mateo.jpg"
                                     alt="Oficina de Arquitectura"
                                     width={1200}
                                     height={900}
